858bef338965df50b64038c8742ce65f530d8984
Leonid <lleonidk@ukr.net>
2026-03-23 16:54:57 +0200

    atlas.png version

---
+++ generate_json.py
@@ -0,0 +1,43 @@
+import json
+import random
+import os
+
+def generate():
+    tiles_dir = r'c:\IntelijIdeaProjects\test-parsing\test-moon\src\main\resources\tiles'
+    tiles = [f for f in os.listdir(tiles_dir) if f.endswith(('.jpg', '.png', '.jpeg'))]
+    
+    total_areas = 1600
+    num_users = 150
+    
+    # Each user gets total_areas / num_users groups
+    groups_per_user = total_areas // num_users
+    extra_groups = total_areas % num_users
+    
+    users = []
+    current_area_id = 1
+    
+    for i in range(1, num_users + 1):
+        user_groups = []
+        num_groups = groups_per_user + (1 if i <= extra_groups else 0)
+        
+        for j in range(1, num_groups + 1):
+            user_groups.append({
+                "groupId": j,
+                "url": "https://google.com",
+                "tile": random.choice(tiles),
+                "areaIds": [current_area_id]
+            })
+            current_area_id += 1
+            
+        users.append({
+            "userId": i,
+            "groups": user_groups
+        })
+        
+    data = {"users": users}
+    
+    with open(r'c:\IntelijIdeaProjects\test-parsing\test-moon\src\main\resources\user\userAreas.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+if __name__ == '__main__':
+    generate()
--- src/main/java/lk/tech/testmoon/service/AtlasService.java
+++ src/main/java/lk/tech/testmoon/service/AtlasService.java
@@ -25,8 +25,17 @@ public class AtlasService {
     private final String atlasJsonPath;
     private final String tilesDirPath;
 
+    //16k
     private static final int ATLAS_WIDTH = 15360;
     private static final int ATLAS_HEIGHT = 8640;
+
+    //8k 7680x4320
+//    private static final int ATLAS_WIDTH = 7680;
+//    private static final int ATLAS_HEIGHT = 4320;
+
+    //4k 3840x2160
+//    private static final int ATLAS_WIDTH = 3840;
+//    private static final int ATLAS_HEIGHT = 2160;
 
     public AtlasService(UserAreaRepository userAreaRepository,
                         @Value("${app.sphere-path:src/main/resources/sphere.json}") String sphereFilePath,
